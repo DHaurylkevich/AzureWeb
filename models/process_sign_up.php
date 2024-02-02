0740642c9fa6b76dc8c243d9057b8c40ff60755a
DHaurylkevich <127322839+DHaurylkevich@users.noreply.github.com>
2024-02-02 18:13:27 +0100

    test 14.2

--- models/process_sign_up.php
+++ models/process_sign_up.php
@@ -5,40 +5,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fields_required = ["username", "email", "password", "cpassword"];
     $errors = [];
 
-//    foreach ($fields_required as $field) {
-//        if (empty($_POST[$field])) {
-//            $errors[] = "Pole <b>$field</b> jest wymagane";
-//        }
-//    }
-//    if (!empty($errors)) {
-//        $_SESSION["error_message"] = implode("<br>", $errors);
-//        header("Location: ../views/registration_form.php");
-//        exit();
-//    }
-//
-//    //Email
-//    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-//
-//    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-//        $_SESSION["error_message"] = "Niepoprawny adress";
-//        header("Location: registration_form.php");
-//        exit();
-//    }
-//
-//    //Pass
-//    if ($_POST["password"] != $_POST["cpassword"]) {
-//        $_SESSION["error_message"] = "Hasła muszą być identyczne";
-//        header("Location: ../views/registration_form.php");
-//        exit();
-//    } elseif (strlen($_POST["password"]) < 8) {
-//        $_SESSION["error_message"] = "Password 8 symbols.";
-//        header("Location: ../views/registration_form.php");
-//        exit();
-//    } elseif (!preg_match("/[A-Z]/", $_POST["password"]) || !preg_match("/[a-z]/", $_POST["password"]) || !preg_match("/[0-9]/", $_POST["password"])) {
-//        $_SESSION["error_message"] = "The password must contain at least one uppercase letter, one lowercase letter, and one number.";
-//        header("Location: ../views/registration_form.php");
-//        exit();
-//    }
+    foreach ($fields_required as $field) {
+        if (empty($_POST[$field])) {
+            $errors[] = "Pole <b>$field</b> jest wymagane";
+        }
+    }
+
+    if (!empty($errors)) {
+        $_SESSION["error_message"] = implode("<br>", $errors);
+        header("Location: ../views/registration_form.php");
+        exit();
+    }
+
+    $email = $_POST["email"];
+
+    //Pass
+    if ($_POST["password"] != $_POST["cpassword"]) {
+        $_SESSION["error_message"] = "Hasła muszą być identyczne";
+        header("Location: ../views/registration_form.php");
+        exit();
+    }
 
     //Username
     $username = htmlspecialchars(trim($_POST["username"]));
@@ -70,7 +56,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($result === FALSE) {
         $_SESSION["error_message"] = "Nie udało się zarejestrować użytkownika";
         echo("Nie udało się zarejestrować użytkownika1");
-        //header("Location: ../views/registration_form.php");
+        header("Location: ../views/registration_form.php");
         exit();
     }
 
@@ -90,9 +76,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
         exit();
     } else {
-        echo("Nie udało się zarejestrować użytkownika: " . $resultData['status']);
-        //$_SESSION["error_message"] = "Nie udało się zarejestrować użytkownika: " . $resultData['message'];
-        //header("Location: ../views/registration_form.php");
+        $_SESSION["error_message"] = "Nie udało się zarejestrować użytkownika: " . $resultData['message'];
+        header("Location: ../views/registration_form.php");
         exit();
     }
 }
