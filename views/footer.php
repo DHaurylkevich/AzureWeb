d82372d212b94aed07911db0a105d9263d3045d7
Daniil Kaminskyi <98901710+whit33th@users.noreply.github.com>
2024-02-03 17:17:09 +0100

    footer fix

--- views/footer.php
+++ views/footer.php
@@ -4,10 +4,10 @@
 
         <div class="help-link">
             <div class="help-link-page">
-                <a href="../html/News.php">News</a>
-                <a href="../html/Sales.php">Sales</a>
-                <a href="../html/FAQ.php">FAQ</a>
-                <a href="../html/Contacts.php">Contacts</a>
+                <a href="../views/News.php">News</a>
+                <a href="../views/Sales.php">Sales</a>
+                <a href="../views/FAQ.php">FAQ</a>
+                <a href="../views/Contacts.php">Contacts</a>
             </div>
             <div class="gap-ten">
                 <a><img class="ico-inst" src="https://static-00.iconduck.com/assets.00/instagram-icon-512x512-ggh8x3cn.png"></a>
